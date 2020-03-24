257c66e6af97a02c5a89bd2ecc5b1fa88183361e
kwizl <karis.njoroge@gmail.com>
2020-03-24 21:06:43 +0300

    Add test for myany? and my_none?

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,7 +1,9 @@
 require './enumerables.rb'
 
-RSpec.describe Enumerable do 
+RSpec.describe Enumerable do
   let(:test_arr) { [2, nil, 6, 8] }
+  let(:all_arr) { [2, 10, 6, 8, 4] }
+
   describe('my_each') do
     it('Should return the Array to_enum when NO BLOCK_GIVEN') do
       actual = test_arr.my_each
@@ -40,20 +42,17 @@ RSpec.describe Enumerable do
       expect(arr_one).to eq(arr_two)
     end
   end
-
   describe('#my_each_with_index') do
     it 'Should return key value pairs of a hash when looped over' do
       arr_one = test_arr.my_each_with_index do |v, k|
         p "#{k}: #{v}"
       end
-      
       arr_two = test_arr.each_with_index do |v, k|
         p "#{k}: #{v}"
       end
       expect(arr_one).to eq(arr_two)
     end
   end
-
   describe('#my_select') do
     it('Should return self to_enum when NO BLOCK_GIVEN') do
       actual = test_arr.my_select
@@ -61,23 +60,75 @@ RSpec.describe Enumerable do
     end
 
     let(:select_arr) { [2, 10, 6, 8, 4] }
-    it('Should return same value as the Generic Ruby select method when BLOCK_GIVEN') do 
-      custom_selects = select_arr.my_select{ |s| s > 2 }      
-      generic_selects = select_arr.select{ |t| t > 2 }
-
+    it('Should return same value as the Generic Ruby select method when BLOCK_GIVEN') do
+      custom_selects = select_arr.my_select { |s| s > 2 }
+      generic_selects = select_arr.select { |t| t > 2 }
       expect(custom_selects).to eq(generic_selects)
     end
   end
-
-  let(:all_arr) { [2, 10, 6, 8, 4] }
-  describe('#my_all') do 
-    it('Should return same value as the Generic Ruby all method when BLOCK_GIVEN') do 
-      custom_alls = all_arr.my_all?{ |s| s > 4 }      
-      generic_alls = all_arr.all?{ |t| t > 4 }
-
-      expect(custom_alls).to eq(generic_alls)
+  describe('#my_all?') do
+    it('Should return same value as the Generic Ruby all method when BLOCK_GIVEN') do
+      custom_all = all_arr.my_all? { |s| s > 4 }
+      generic_all = all_arr.all? { |t| t > 4 }
+      expect(custom_all).to eq(generic_all)
     end
   end
-
-
+  describe('#my_any?') do
+    it('Should return same value as the Generic Ruby any method when BLOCK_GIVEN') do
+      custom_any = all_arr.my_any? { |s| s > 4 }
+      generic_any = all_arr.any? { |t| t > 4 }
+      expect(custom_any).to eq(generic_any)
+    end
+    it('Should return same value as the Generic Ruby any method when given a PARAMETER') do
+      custom_any = all_arr.my_any?(1)
+      generic_any = all_arr.any?(1)
+      expect(custom_any).to eq(generic_any)
+    end
+    it('Should return same value as the Generic Ruby any method when given CLASS value') do
+      custom_any = all_arr.my_any?(Integer)
+      generic_any = all_arr.any?(Integer)
+      expect(custom_any).to eq(generic_any)
+    end
+    let(:any_string_arr) { %w[ant bear cat] }
+    it('Should return same value as the Generic Ruby any method when given a REGEXP') do
+      custom_any = any_string_arr.my_any?(/t/)
+      generic_any = any_string_arr.any?(/t/)
+      expect(custom_any).to eq(generic_any)
+    end
+    let(:any_values) { [nil, true, 99] }
+    it('Should return same value as the Generic Ruby any method when given RANDOM values without PARAMETER') do
+      custom_any = any_values.my_any?
+      generic_any = any_values.any?
+      expect(custom_any).to eq(generic_any)
+    end
+  end
+  describe('#my_none?') do
+    it('Should return same value as the Generic Ruby any method when BLOCK_GIVEN') do
+      custom_none = all_arr.my_none? { |s| s > 4 }
+      generic_none = all_arr.none? { |t| t > 4 }
+      expect(custom_none).to eq(generic_none)
+    end
+    it('Should return same value as the Generic Ruby none method when given a PARAMETER') do
+      custom_none = all_arr.my_none?(1)
+      generic_none = all_arr.none?(1)
+      expect(custom_none).to eq(generic_none)
+    end
+    it('Should return same value as the Generic Ruby none method when given CLASS value') do
+      custom_none = all_arr.my_none?(Integer)
+      generic_none = all_arr.none?(Integer)
+      expect(custom_none).to eq(generic_none)
+    end
+    let(:none_string_arr) { %w[ant bear cat] }
+    it('Should return same value as the Generic Ruby none method when given a REGEXP') do
+      custom_none = none_string_arr.my_none?(/t/)
+      generic_none = none_string_arr.none?(/t/)
+      expect(custom_none).to eq(generic_none)
+    end
+    let(:none_values) { [nil, true, 99] }
+    it('Should return same value as the Generic Ruby none method when given RANDOM values without PARAMETER') do
+      custom_none = none_values.my_none?
+      generic_none = none_values.none?
+      expect(custom_none).to eq(generic_none)
+    end
+  end
 end
